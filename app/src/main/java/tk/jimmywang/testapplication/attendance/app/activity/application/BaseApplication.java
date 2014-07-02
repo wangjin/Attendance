090d8c51edb404393517f5e93424fdf635663afc
wangjin <wangjin-252@hotmail.com>
2014-07-02 21:19:04 +0800

    添加BaseApplication

---
+++ app/src/main/java/tk/jimmywang/testapplication/attendance/app/activity/application/BaseApplication.java
@@ -0,0 +1,13 @@
+package tk.jimmywang.testapplication.attendance.app.activity.application;
+
+import android.app.Application;
+
+/**
+ * Created by WangJin on 2014/7/2.
+ */
+public class BaseApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
