44b5462675ef12f5e31fe6b2321236dbe7ced1d1
wangjin <wangjin-252@hotmail.com>
2014-07-04 19:31:42 +0800

    测试ProgressBar

--- app/src/main/java/tk/jimmywang/attendance/app/activity/MainActivity.java
+++ app/src/main/java/tk/jimmywang/attendance/app/activity/MainActivity.java
@@ -1,18 +1,23 @@
 package tk.jimmywang.attendance.app.activity;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import tk.jimmywang.testapplication.attendance.app.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Test().execute();
+
     }
 
 
@@ -34,4 +39,31 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    class Test extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String str) {
+            super.onPostExecute(str);
+            RelativeLayout relativeLayout  = (RelativeLayout) findViewById(R.id.progressbar_parent);
+            relativeLayout.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+    }
 }
+
