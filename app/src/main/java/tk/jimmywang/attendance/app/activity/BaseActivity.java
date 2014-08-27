58e5e4ff1983ae6177bf22cc1597f95d98a38865
wangjin <wangjin-252@hotmail.com>
2014-08-27 21:41:03 +0800

    requestWindowFeature(Window.FEATURE_NO_TITLE)修改为   supportRequestWindowFeature(Window.FEATURE_NO_TITLE)

--- app/src/main/java/tk/jimmywang/attendance/app/activity/BaseActivity.java
+++ app/src/main/java/tk/jimmywang/attendance/app/activity/BaseActivity.java
@@ -14,7 +14,7 @@ public class BaseActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //去掉标题
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE); //去掉标题
     }
 
     public void getLogger(String tag,String msg){
