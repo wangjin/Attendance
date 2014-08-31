ea06cac003a5d66377a1789248c234e860f8421e
wangjin <wangjin-252@hotmail.com>
2014-08-31 20:51:55 +0800

    移动REQUEST_URL到Common类中

--- app/src/main/java/tk/jimmywang/attendance/app/fragment/BaseFragment.java
+++ app/src/main/java/tk/jimmywang/attendance/app/fragment/BaseFragment.java
@@ -7,8 +7,4 @@ import android.support.v4.app.Fragment;
  */
 public class BaseFragment extends Fragment {
 
-    public static final String REQUEST_URL = "http://192.168.1.111:8080/attendance";
-
-
-
 }
---
+++ app/src/main/java/tk/jimmywang/attendance/app/util/Common.java
@@ -0,0 +1,9 @@
+package tk.jimmywang.attendance.app.util;
+
+/**
+ * Created by 金 on 2014/8/31.
+ */
+public class Common {
+
+    public static final String REQUEST_URL = "http://192.168.1.111:8080/attendance";
+}
